2799779c970fc8347eb1b35228f8edb9b89e7b4a
Doug Voss <douglas.c.voss@gmail.com>
2015-04-14 17:52:05 -0600

    Big Nerd Ranch Chapter 7 Silver Challenge

---
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,121 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by John Gallagher on 1/6/14.
+//  Copyright (c) 2014 John Gallagher. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+#import "BNRHypnosisView.h"
+
+
+@interface BNRHypnosisViewController () <UITextFieldDelegate>
+@end
+
+
+@implementation BNRHypnosisViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+        // Set the tab bar item's title
+        self.tabBarItem.title = @"Hypnotize";
+
+        // Create a UIImage from a file
+        // This will use Hypno@2x on retina display devices
+        UIImage *image = [UIImage imageNamed:@"Hypno.png"];
+
+        // Put that image on the tab bar item
+        self.tabBarItem.image = image;
+    }
+
+    return self;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    //NSLog(@"%@", textField.text);
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)loadView
+{
+    // Create a view
+    CGRect frame = [UIScreen mainScreen].bounds;
+    BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    
+    textField.delegate = self;
+    
+    [backgroundView addSubview:textField];
+
+    // Set it as *the* view of this view controller
+    self.view = backgroundView;
+}
+
+- (void)viewDidLoad
+{
+    // Always call the super implementation of viewDidLoad
+    [super viewDidLoad];
+
+    NSLog(@"BNRHypnosisViewController loaded its view");
+}
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++) {
+        
+        UILabel *messageLabel = [[UILabel alloc] init];
+        
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        
+        [messageLabel sizeToFit];
+        
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        int x = arc4random() % width;
+        
+        int height = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        int y = arc4random() % height;
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *motionEffect;
+        motionEffect = [[UIInterpolatingMotionEffect alloc]
+                        initWithKeyPath:@"center.x"
+                        type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc]
+                        initWithKeyPath:@"center.y"
+                        type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        
+                                                                             
+    }
+}
+
+@end
